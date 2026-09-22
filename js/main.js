e8c6adcbd82eac3cf1b57f8939fed337ee1fe570
adjisay <pe_1n@mail.ru>
2026-09-22 20:16:15 +0300

    feat: добавить генерацию данных фотографий и комментариев

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const descriptions = [
+  'Красивый закат над морем',
+  'Прогулка по вечернему городу',
+  'Мой любимый кот',
+  'Отличный день на природе',
+  'Незабываемое путешествие',
+];
+
+const names = [
+  'Александр',
+  'Евгения',
+  'Руслан',
+  'Татьяна',
+  'Максим',
+  'Светлана',
+  'Артём',
+  'Мария',
+  'Иван',
+  'Анна',
+  'Дмитрий',
+  'Екатерина',
+];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+];
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const getRandomMessage = () => {
+  const firstMessage = getRandomArrayElement(messages);
+
+  if (getRandomInteger(1, 2) === 1) {
+    return firstMessage;
+  }
+
+  const secondMessage = getRandomArrayElement(messages);
+
+  return `${firstMessage} ${secondMessage}`;
+};
+
+const createComment = (id) => ({
+  id,
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+  message: getRandomMessage(),
+  name: getRandomArrayElement(names),
+});
+
+const createComments = () => Array.from({ length: getRandomInteger(0, 30) }, (_, index) => createComment(index + 1));
+
+const createPhoto = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: getRandomArrayElement(descriptions),
+  likes: getRandomInteger(15, 200),
+  comments: createComments(),
+});
+
+const photos = Array.from({ length: 25 }, (_, index) => createPhoto(index + 1));
